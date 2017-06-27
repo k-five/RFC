b8a41caae09ebfb0f8124bc84b2eccc0b76d7609
Shekib <vhsu30@yahoo.com>
2017-06-27 16:29:29 +0430

    add valid header file

---
+++ cpp/rfc/valid.hpp
@@ -0,0 +1,122 @@
+/*
+    +-------------------------------------+
+    |Regex Function Collection ( or: RFC )|
+    |-------------------------------------|
+    |Written by Shakiba (=k-five) in 2017,|
+    |shared on Git-Hub .../k-five/RFC,    |
+    |All things that is used, is from std,|
+    |there is no nonstandard library, but,|
+    |you can use boost in place of std.   |
+    +-------------------------------------+
+    |Any bug, suggestion, critic, opinion,|
+    |would be appreciated. Feel free if y-|
+    |ou need a special function that you c|
+    |ould not find it here.               |
+    +-------------------------------------+
+    | contact me: vhsu30@yahoo.com        |
+    | github: k-five                      |
+    | stackoverflow: k-five               |
+    | twitter: k__five                    |
+    +-------------------------------------+
+
+    =======================================
+
+    content:
+
+    valid family:
+        regex_is_valid (1)
+        regex_is_valid (2)
+
+    parse family:
+        parse
+        parse_to
+        parse_count
+        parse_index
+        parse_index_to
+        parse_index_all
+        parse_index_all_to
+        parse_option
+        parse_option_to
+        parse_option_all
+        parse_option_all_to
+
+    split family:
+        split
+        split_to
+
+    join family:
+        join
+        join_to
+
+    substitute family:
+        substitute
+        substitute_to
+        substitute_all
+        substitute_all_to
+        substitute_at_index
+        substitute_at_index_to
+
+    match family:
+        match
+        match_to
+        match_all
+        match_all_to
+        match_at_index
+        match_at_index_to
+        has_match
+
+    get family:
+        get_prefix
+        get_prefix_to
+        get_suffix
+        get_suffix_to
+        get_prefix_of_index
+        get_prefix_of_index_to
+        get_suffix_of_index
+        get_suffix_of_index_to
+
+    count family
+        count_match
+*/
+
+#ifndef RFC_VALID_HPP
+#define RFC_VALID_HPP
+
+#include <regex>
+#include <string>
+
+namespace k5{
+
+/***********************************************************************/
+/*                                                                     */
+/*                            valid family                             */
+/*                                                                     */
+/***********************************************************************/
+
+bool regex_is_valid( const std::string& user_pattern ){
+    bool result = false;
+
+    try{
+        std::regex regex( user_pattern );
+        result = true;
+    } catch( const std::regex_error& ){
+        result = false;
+    }
+
+    return result;
+}
+
+const char* regex_is_valid( const char* user_pattern ){
+    const char* result = nullptr;
+    try{
+        std::regex regex( user_pattern );
+    } catch( const std::regex_error& re ){
+        result = re.what();
+    }
+
+    return result;
+}
+
+} // end of namespace k5
+
+#endif // RFC_VALID_HPP
